8f1b10da663e0d6f3543f6a9dc8d72a4048b7ca5
rahul30824 <rahulr684.sse@saveetha.com>
2025-01-27 23:00:32 +0530

    Create Element Appears once.java

---
+++ Element Appears once.java
@@ -0,0 +1,18 @@
+class Main{
+    public static int singleNonDuplicate(int[] nums) {
+        int target = 0;
+
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];             }
+        }
+
+        return nums[nums.length - 1];
+    }
+  public static void main(String[] args)  
+  {
+      int[] array={1,1,2,2,4,5,5,6};
+      int result=singleNonDuplicate(array);
+      System.out.print(result);
+  }
+}
