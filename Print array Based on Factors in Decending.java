ffd2a5d1624312646fb4515f77e5ca9404025910
rahul30824 <rahulr684.sse@saveetha.com>
2025-02-10 22:54:52 +0530

    Print array Based on Factors in Decending.java

---
+++ Print array Based on Factors in Decending.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Main {
+    public static int CountDivisors(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {20, 12, 4, 5, 7, 19, 7};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+
+        // Count divisors and store in map
+        for (int num : array) {
+            int count = CountDivisors(num);
+            map.put(num, count);
+            list.add(num);
+        }
+
+        // Convert map to a list of entries and sort by divisor count in descending order
+        List<Map.Entry<Integer, Integer>> entries = new ArrayList<>(map.entrySet());
+        entries.sort((entry1, entry2) -> entry2.getValue() - entry1.getValue());
+
+        // Print the values in descending order based on divisor count
+        for (Map.Entry<Integer, Integer> entry : entries) {
+            System.out.print(entry.getKey()+",");
+        }
+    }
+}
