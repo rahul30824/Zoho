711e71dee833af9b42cffa546677083b2e50d070
Rahul_Ramamoorthy:) <rahulr684.sse@saveetha.com>
2025-02-13 20:29:24 +0530

    Permutation.java

---
+++ Permutation.java
@@ -0,0 +1,48 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+
+  public List<List<Integer>> permute(int[] nums) {
+    List<List<Integer>> resultList = new ArrayList<>();
+    backtrack(resultList, new ArrayList<>(), nums);
+    return resultList;
+  }
+
+  private void backtrack(List<List<Integer>> resultList, ArrayList<Integer> tempList, int[] nums) {
+    // If we match the length, it is a permutation
+    if (tempList.size() == nums.length) {
+      resultList.add(new ArrayList<>(tempList));
+      return;
+    }
+
+    for (int number : nums) {
+      // Skip if we get same element
+      if (tempList.contains(number))
+        continue;
+
+      // Add the new element
+      tempList.add(number);
+
+      // Go back to try other element
+      backtrack(resultList, tempList, nums);
+
+      // Remove the element
+      tempList.remove(tempList.size() - 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    Permutations permutations = new Permutations();
+    int[] nums = {1, 2, 3}; // Example input
+    List<List<Integer>> result = permutations.permute(nums);
+    
+    // Printing the result
+    System.out.println("Permutations of the array: ");
+    for (List<Integer> perm : result) {
+      System.out.println(perm);
+    }
+  }
+}
