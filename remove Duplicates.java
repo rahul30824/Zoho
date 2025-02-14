a3931b1caaa86f2013db7e3a3ae2247a8e81b7f2
Rahul_Ramamoorthy:) <rahulr684.sse@saveetha.com>
2025-02-14 20:57:13 +0530

    remove Duplicates.java

--- remove Duplicates.java
+++ remove Duplicates.java
@@ -1,24 +1,26 @@
-class Main{
-    public static int removeDuplicates(int[] array)
-    {
-        int j=0;
-       for(int i=0;i<array.length-1;i++) 
-       {
-           if(array[i]!=array[i+1])
-           {
-              array[j++] =array[i];
-           }
-           
-       }
-       array[j++]=array[array.length-1];
-       return j;
+import java.util.*;
+
+class Main {
+    
+    public static int[] Solution(int[] array) {
+        int j = 0;
+        int[] newarr = new int[array.length];  
+        HashSet<Integer> set = new HashSet<>();
+        
+        // Traverse through the array and add unique elements to the set
+        for (int num : array) {
+            if (!set.contains(num)) {
+                set.add(num);  // Add to set if not already present
+                newarr[j++] = num;  // Add to newarr and increment the index
+            }
+        }
+        
+        // Return only the part of the array that contains unique elements
+        return Arrays.copyOf(newarr, j);
     }
-public static void main(String[] args)
-{
-    int [] array={1,2,2,3,3,4,5,5};
-    int result=removeDuplicates(array);
-    for(int i=0;i<result;i++){
-    System.out.print(array[i]);
+
+    public static void main(String[] args) {
+        int[] array = {1, 3, 4, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4};
+        System.out.println(Arrays.toString(Solution(array)));  // Output unique elements
     }
-}
 }
