da438d6a19b4e49f06db8f22b63698eedd7c8353
Rahul_Ramamoorthy:) <rahulr684.sse@saveetha.com>
2025-02-14 20:58:56 +0530

    remove Duplicates.java

--- remove Duplicates.java
+++ remove Duplicates.java
@@ -3,20 +3,19 @@ import java.util.*;
 class Main {
     
     public static int[] Solution(int[] array) {
-        int j = 0;
-        int[] newarr = new int[array.length];  
+        int j=0;  
         HashSet<Integer> set = new HashSet<>();
         
         // Traverse through the array and add unique elements to the set
         for (int num : array) {
             if (!set.contains(num)) {
                 set.add(num);  // Add to set if not already present
-                newarr[j++] = num;  // Add to newarr and increment the index
+                array[j++] = num;  // Add to newarr and increment the index
             }
         }
         
         // Return only the part of the array that contains unique elements
-        return Arrays.copyOf(newarr, j);
+        return Arrays.copyOf(array, j);
     }
 
     public static void main(String[] args) {
