741fa8349a78a01d9c28c0d6f8406771751be97d
Rahul_Ramamoorthy:) <rahulr684.sse@saveetha.com>
2025-02-15 10:58:23 +0530

    Prime_number.java

--- Prime_number.java
+++ Prime_number.java
@@ -24,3 +24,39 @@ class Main{
      System.out.print(IsPrime(Num));
     }
 }
+
+
+
+
+
+class Main{
+    public static boolean Solution(int num)
+    {
+       if(num<=1)
+       {
+           return false;
+       }
+        if(num==2)
+       {
+           return true;
+       }
+      for(int i=2;i*i<=num;i++)
+      {
+          if(num%i==0)
+          {
+              return false;
+          }
+      }
+      return true;
+    }
+    
+    public static void main(String[] args)
+    {
+        int[] array={5,21,7,26,7,21};
+       for(int num:array)
+       {
+           boolean result=Solution(num);
+           System.out.println(result);
+       }
+    }
+}
