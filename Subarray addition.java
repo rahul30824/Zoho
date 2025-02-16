e0fe4f5b980cf90fb4acc72063f2b1b083f20dd8
Rahul_Ramamoorthy:) <rahulr684.sse@saveetha.com>
2025-02-16 15:31:45 +0530

    Subarray addition.java

---
+++ Subarray addition.java
@@ -0,0 +1,23 @@
+class Main {
+    public static int Solution(int[] array) {
+        int j = 0;
+        int currentsum = 0;
+        int maxsum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            currentsum += array[i];
+            maxsum = Math.max(currentsum, maxsum);
+            
+            while (currentsum < 0) { // Fix: Reset subarray when sum goes negative
+                currentsum -= array[j]; 
+                j++; // Move left boundary
+            }
+        }
+        return maxsum;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 8,-4, -5,15};
+        System.out.print(Solution(array)); // Output: 13
+    }
+}
