42c837a567fe61df2e2adb41375b7b6e667fa94d
Rahul_Ramamoorthy:) <rahulr684.sse@saveetha.com>
2025-02-22 20:47:50 +0530

    NextPermutation.java

---
+++ NextPermutation.java
@@ -0,0 +1,39 @@
+
+class Solution{
+    public static int[] nextPermutation(int [] nums)
+    {
+     int n=nums.length;
+     int i=n-2;
+     while(i>=0 && nums[i]>=nums[i+1])
+     {
+         i--;    //pivot find//
+     }
+     if(i>=0)
+     {
+         int j=n-1;
+         while(nums[j]<=nums[i])
+         {
+             j--;
+         }
+         swap(nums,i,j);
+     }
+     reverse(nums,i+1,n-1);
+     return nums;
+    }
+   
+     private static void swap(int[] nums,int i,int j)
+     {
+         int temp=nums[i];
+         nums[i]=nums[j];
+         nums[j]=temp;
+     }
+     private static void reverse(int[] nums,int start,int end)
+     {
+         while(start<end)
+         {
+             swap(nums,start++,end--);
+         }
+     }
+}
+
+
