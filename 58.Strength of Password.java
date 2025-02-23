525041c2e7449972820790391a2cc0cf503185d3
Rahul_Ramamoorthy:) <rahulr684.sse@saveetha.com>
2025-02-23 16:28:07 +0530

    58.Strength of Password.java

--- 58.Strength of Password.java
+++ 58.Strength of Password.java
@@ -1,50 +1,37 @@
-
-import java.io.*;
 import java.util.*;
+import java.util.regex.*;
 
-class GFG {
-  
-  
-    public static void printStrongNess(String input)
-    {
-        // Checking lower alphabet in string
+class Main {
+    public static void Solution(String input) {
         int n = input.length();
-        boolean hasLower = false, hasUpper = false,
-                hasDigit = false, specialChar = false;
-        Set<Character> set = new HashSet<Character>(
-            Arrays.asList('!', '@', '#', '$', '%', '^', '&',
-                          '*', '(', ')', '-', '+'));
-        for (char i : input.toCharArray())
-        {
-            if (Character.isLowerCase(i))
-                hasLower = true;
-            if (Character.isUpperCase(i))
-                hasUpper = true;
-            if (Character.isDigit(i))
-                hasDigit = true;
-            if (set.contains(i))
-                specialChar = true;
+        
+        // Check if the password contains a lowercase letter
+        boolean hasLower = Pattern.compile("[a-z]").matcher(input).find();
+        
+        // Check if the password contains an uppercase letter
+        boolean hasUpper = Pattern.compile("[A-Z]").matcher(input).find();
+        
+        // Check if the password contains a digit
+        boolean hasDigit = Pattern.compile("[0-9]").matcher(input).find();
+        
+        // Check if the password contains a special character
+        boolean hasSpecial = Pattern.compile("[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]").matcher(input).find();
+        
+        System.out.println("Strength of Password");
+        
+        if (hasLower && hasUpper && hasDigit && hasSpecial && n >= 8) {
+            System.out.print("Hard");
+        } else if ((hasLower || hasUpper || hasDigit) && n >= 6) {
+            System.out.print("Moderate");
+        } else {
+            System.out.print("Weak");
         }
-      
-        // Strength of password
-        System.out.print("Strength of password:- ");
-        if (hasDigit && hasLower && hasUpper && specialChar
-            && (n >= 8))
-            System.out.print(" Strong");
-        else if ((hasLower || hasUpper || specialChar)
-                 && (n >= 6))
-            System.out.print(" Moderate");
-        else
-            System.out.print(" Weak");
     }
 
-    // Driver Code
-    public static void main(String[] args)
-    {
-        String input = "Ragu";
-        printStrongNess(input);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your Password");
+        String input = scanner.nextLine();
+        Solution(input);
     }
-
-    
 }
-// contributed by Ashish Chhabra
